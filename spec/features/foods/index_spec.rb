bb6973e6197c02af3beea501d3ed4b2c5b0b625a
Melony Franchini <meltravelz@gmail.com>
2023-04-07 10:23:28 -0600

    Create foods index view & test & controller

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -5,17 +5,23 @@ RSpec.describe "/foods", type: :feature do
     it "displays total count, 10 foods with 4 attributes each" do
       visit root_path
       
-      fill_in("q", with: "sweet potatoes")
+      fill_in("food_word", with: "sweet potatoes")
       click_on("Search")
 
       expect(current_path).to eq("/foods")
 
-      expect(page).to have_content("Top 10 Foods")
+      expect(page).to have_content("Search Results")
       expect(page).to have_content("Total Count: 38114")
+      
+      expect(page).to have_content("Top 10 Foods:")
+      expect(page).to have_content("GTIN/UPC Code:", count: 10)
 
-      expect(page).to have_content("GTIN/UPC code:", count: 10)
-
-      # within #
+      within "#food-728229015529" do
+        expect(page).to have_content("GTIN/UPC Code: 728229015529")
+        expect(page).to have_content("Description: Sweets medley sel gris, batata, sweet potato, purple sweet potato real vegetable chips, sweets medley")
+        expect(page).to have_content("Brand: The Hain Celestial Group, Inc.")
+        expect(page).to have_content("Ingredients: A blend of sweet potatoes (sweet potato, purple sweet potato, batata), avocado oil, sea salt.")
+      end
     end
   end
 end
