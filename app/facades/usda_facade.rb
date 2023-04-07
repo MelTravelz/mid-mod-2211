25b7e1a9c4a41d37949f05ed1ecd4053bc603852
Melony Franchini <meltravelz@gmail.com>
2023-04-07 09:50:00 -0600

    Create #find_10_foods_by_ingredient facade method

---
+++ app/facades/usda_facade.rb
@@ -0,0 +1,27 @@
+require 'ostruct'
+
+class UsdaFacade
+  attr_reader :usda_service
+
+  def initialize
+    @usda_service = UsdaService.new
+  end
+
+  def find_10_foods_by_ingredient(food_word)
+    usda_service.get_10_foods(food_word)[:foods].map do |food_info|
+      OpenStruct.new(
+        gtin_upc: food_info[:gtinUpc],
+        description: food_info[:description].capitalize,
+        brand_owner: food_info[:brandOwner],
+        ingredients: food_info[:ingredients].capitalize
+      )
+    end
+  end
+
+  # Maybe this will work: 
+  # def find_total_count_by_ingredient(food_word)
+  #   food_info = usda_service.get_10_foods(food_word)
+  #   food_info[:totalHits]
+  # end
+ 
+end
--- spec/facades/usda_facade_spec.rb
+++ spec/facades/usda_facade_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe UsdaFacade do
   describe "instance methods" do
+
     context "#find_10_foods_by_ingredient" do
-      it "returns an array of food objects" do
+      it "returns an array of OpenStruct objects that have w/ 4 attributes" do
         food = UsdaFacade.new
 
         expect(food.find_10_foods_by_ingredient("sweet potatoes")).to be_an(Array)
+        expect(food.find_10_foods_by_ingredient("sweet potatoes").first).to be_a(OpenStruct)
+
+        expect(food.find_10_foods_by_ingredient("sweet potatoes").first.gtin_upc).to eq("728229015529")
+        expect(food.find_10_foods_by_ingredient("sweet potatoes").first.description).to eq("Sweets medley sel gris, batata, sweet potato, purple sweet potato real vegetable chips, sweets medley")
+        expect(food.find_10_foods_by_ingredient("sweet potatoes").first.brand_owner).to eq("The Hain Celestial Group, Inc.")
+        expect(food.find_10_foods_by_ingredient("sweet potatoes").first.ingredients).to eq("A blend of sweet potatoes (sweet potato, purple sweet potato, batata), avocado oil, sea salt.")
       end
     end
+
   end
 end
