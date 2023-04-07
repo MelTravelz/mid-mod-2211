200c859afbae117fae0886a1a010b3bd471f3287
Melony Franchini <meltravelz@gmail.com>
2023-04-07 09:22:16 -0600

    Create foods search service/endpoint connection

---
+++ app/services/usda_service.rb
@@ -0,0 +1,15 @@
+class UsdaService
+  def fetch_api(url)
+    response = connection.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_search(food_word)
+    fetch_api("foods/search?pageSize=10&ingredients=#{food_word}")
+  end
+  
+  private
+  def connection
+    Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/", params: { api_key: ENV["api_key"] })
+  end
+end
---
+++ spec/services/usda_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe UsdaService do
+  describe "instance methods" do 
+    context "#fetch_api" do
+      it "#get_search(food_word)" do
+        food_service = UsdaService.new
+        food = food_service.get_search("sweet potatoes")
+        
+        expect(food).to be_a(Hash)
+        expect(food.keys.count).to eq(7)
+        # expect(food.keys).to eq([:totalHits, :currentPage, :totalPages, :pageList, :foodSearchCriteria, :foods, :aggregations])
+      end
+    end 
+  end
+end
